e48d470c44fe5d125267aed346b8a6d46ba98b11
Nitish2656 <n.nitishkumar4440@gmail.com>
2023-01-24 20:48:03 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Test
+{
+    public static void main (String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int i;
+        System.out.println("Enter 0 to EXIT:");
+        while(true){
+        
+            i=obj.nextInt();
+            if(i==0){
+                System.out.println("Right Answer");
+break;
+            }
+            System.out.println("INVALID"+i);
+        System.out.println("ENTERED AGAIN PLEASE");
+}
+    }
+}
